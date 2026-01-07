9861e6e4887f0153079d8a44e4287578bafa0818
FrankMontgomery063 <montgomeryfrank.fm@gmail.com>
2026-01-07 16:40:24 -0500

    "created person class"

---
+++ src/test/java/com/zipcodewilmington/beanslearnerlab/BeansLearnerLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zipcodewilmington.beanslearnerlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeansLearnerLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
