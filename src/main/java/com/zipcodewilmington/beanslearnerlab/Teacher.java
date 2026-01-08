50ed61e45531e333818ff061abb23cf7e3efd63f
FrankMontgomery063 <montgomeryfrank.fm@gmail.com>
2026-01-08 15:41:27 -0500

    "created teacher interface"

---
+++ src/main/java/com/zipcodewilmington/beanslearnerlab/Teacher.java
@@ -0,0 +1,7 @@
+package com.zipcodewilmington.beanslearnerlab;
+
+public interface Teacher {
+    void teach(Learner learner, double numberOfHours);
+
+    void lecture(Iterable<? extends Learner> learners, double numberOfHours);
+}
